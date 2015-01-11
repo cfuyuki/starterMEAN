ec77fd5ac7e4c3dbfcdad3b50e3ef6dd4738259d
Chie Fuyuki <cfuyuki@gmail.com>
2015-01-11 08:28:35 +0800

    Tutorial Day #4
    http://www.bossable.com/587/mean-stack-angular-intro/
    AngularJS
    ng-repeat, ng-class

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,44 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		$scope.alerts = [
+			{
+				icon: 'glyphicon-user',
+				colour: 'btn-success',
+				total: '20,408',
+				description: 'Total Customer'
+			},
+			{
+				icon: 'glyphicon-calendar',
+				colour: 'btn-primary',
+				total: '8,382',
+				description: 'Upcoming Events'
+			},
+			{
+				icon: 'glyphicon-edit',
+				colour: 'btn-success',
+				total: '527',
+				description: 'New Customer in 2014'
+			},
+			{
+				icon: 'glyphicon-record',
+				colour: 'btn-info',
+				total: '85,000',
+				description: 'Emails Sent'
+			},
+			{
+				icon: 'glyphicon-eye-open',
+				colour: 'btn-warning',
+				total: '268',
+				description: 'Follow up Required'
+			},
+			{
+				icon: 'glyphicon-flag',
+				colour: 'btn-danger',
+				total: '348',
+				description: 'Referrals to Moderate'
+			}
+		];
 	}
 ]);
