133bf0dc37b9dd7a3f1f020281285d2fbcb06506
Chie Fuyuki <cfuyuki@gmail.com>
2015-01-14 14:21:28 +0800

    Mean App 30 Day Challenge

---
+++ config/env/development.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+	db: 'mongodb://cfuyuki:doughGr7@ds031581.mongolab.com:31581/db_mean',
+	app: {
+		title: 'Starter Mean - Development Environment'
+	},
+	facebook: {
+		clientID: process.env.FACEBOOK_ID || 'APP_ID',
+		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+		callbackURL: '/auth/facebook/callback'
+	},
+	twitter: {
+		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
+		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+		callbackURL: '/auth/twitter/callback'
+	},
+	google: {
+		clientID: process.env.GOOGLE_ID || '396928174488-u11jr0fffk4vd1tsan2k4a6fj9fs4icm.apps.googleusercontent.com',
+		clientSecret: process.env.GOOGLE_SECRET || 'NB3-kNrmgGRdnogGQHQ8nb7Q',
+		callbackURL: '/auth/google/callback'
+	},
+	linkedin: {
+		clientID: process.env.LINKEDIN_ID || 'APP_ID',
+		clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
+		callbackURL: '/auth/linkedin/callback'
+	},
+	github: {
+		clientID: process.env.GITHUB_ID || 'APP_ID',
+		clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
+		callbackURL: '/auth/github/callback'
+	},
+	mailer: {
+		from: process.env.MAILER_FROM || 'MAILER_FROM',
+		options: {
+			service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+			auth: {
+				user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
+				pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+			}
+		}
+	}
+};
